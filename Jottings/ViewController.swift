8a53ca2af7a8ccb1b02b4afc6254e3d54a3d87ae
Morten Kals <m.kals@me.com>
2016-08-29 18:25:01 -0700

    Initial Commit

---
+++ Jottings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jottings
+//
+//  Created by Morten Kals on 29/08/2016.
+//  Copyright © 2016 Kals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
